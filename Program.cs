94a2c1643f1ed35ff120e360a21ccd02973d4936
Kauã <kau.lira@yahoo.com>
2023-12-10 23:00:37 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿double D;
+
+Console.WriteLine("Digite o desvio percentual:");
+D= Convert.ToDouble(Console.ReadLine());
+
+if(D >= 5.0){
+  Console.Clear();
+  Console.Beep();
+  Console.ForegroundColor=ConsoleColor.Red;
+  Console.WriteLine("Rejeitado");
+
+  Console.ResetColor();
+}
+
+else if(D >= 3.0){
+  Console.Clear();
+  Console.Beep();
+  Console.ForegroundColor=ConsoleColor.Yellow;
+  Console.WriteLine("3º Classe");
+
+  Console.ResetColor();
+}
+
+else if(D >= 1.0){
+  Console.Clear();
+  Console.Beep();
+  Console.ForegroundColor=ConsoleColor.Blue;
+  Console.WriteLine("2º Classe");
+
+  Console.ResetColor();
+}
+
+else{
+  Console.Clear();
+  Console.Beep();
+  Console.ForegroundColor=ConsoleColor.Blue;
+  Console.WriteLine("1º Classe");
+
+  Console.ResetColor();
+}
